0a574fc2eff82d4a92a835799d816d334899a246
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-02 18:43:08 +0900

    3-4. GETメソッドの送信と、ボディ、ステータス、ヘッダーの受信

---
+++ client_http1/cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://localhost:50510")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(string(body))
+	log.Println("Status: ", res.Status)
+	log.Println("StatusCode: ", res.StatusCode)
+	log.Println("Headers: ", res.Header)
+	log.Println("Content-Length: ", res.Header.Get("Content-Length"))
+}
