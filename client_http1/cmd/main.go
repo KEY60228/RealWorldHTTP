35ede1ebe7412bd75280448bbd745d4719d514d0
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-03 19:37:33 +0900

    3-9. multipart/form-data形式でファイルの送信

--- client_http1/cmd/main.go
+++ client_http1/cmd/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("cmd/main.go")
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Michael Jackson")
+
+	part := make(textproto.MIMEHeader)
+	part.Set("Content-Type", "image/jpeg")
+	part.Set("Content-Disposition", `form-data; name="thumbnail"; filename="photo/photo.jpg"`)
+
+	fileWriter, err := writer.CreatePart(part)
 	if err != nil {
 		panic(err)
 	}
 
-	res, err := http.Post("http://localhost:50510", "text/plain", file)
+	readFile, err := os.Open("photo/photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	res, err := http.Post("http://localhost:50510", writer.FormDataContentType(), &buffer)
 	if err != nil {
 		panic(err)
 	}
