bfed7a6170b472c07f41f8096630dff7b1397fee
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-03 19:42:29 +0900

    3-10. クッキーの送受信

--- client_http1/cmd/main.go
+++ client_http1/cmd/main.go
@@ -1,53 +1,33 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"net/textproto"
-	"os"
+	"net/http/cookiejar"
+	"net/http/httputil"
 )
 
 func main() {
-	var buffer bytes.Buffer
-	writer := multipart.NewWriter(&buffer)
-	writer.WriteField("name", "Michael Jackson")
-
-	part := make(textproto.MIMEHeader)
-	part.Set("Content-Type", "image/jpeg")
-	part.Set("Content-Disposition", `form-data; name="thumbnail"; filename="photo/photo.jpg"`)
-
-	fileWriter, err := writer.CreatePart(part)
+	jar, err := cookiejar.New(nil)
 	if err != nil {
 		panic(err)
 	}
 
-	readFile, err := os.Open("photo/photo.jpg")
-	if err != nil {
-		panic(err)
-	}
-	defer readFile.Close()
-
-	io.Copy(fileWriter, readFile)
-	writer.Close()
-
-	res, err := http.Post("http://localhost:50510", writer.FormDataContentType(), &buffer)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
+	client := http.Client{
+		Jar: jar,
 	}
 
-	log.Println(string(body))
-	log.Println("Status: ", res.Status)
-	log.Println("StatusCode: ", res.StatusCode)
-	log.Println("Headers: ", res.Header)
-	log.Println("Content-Length: ", res.Header.Get("Content-Length"))
+	for i := 0; i < 2; i++ {
+		res, err := client.Get("http://localhost:50510/cookie")
+		if err != nil {
+			panic(err)
+		}
+
+		dump, err := httputil.DumpResponse(res, true)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println(string(dump))
+	}
 }
