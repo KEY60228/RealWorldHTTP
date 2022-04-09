ad221a8e973e00e4d556109c6278523e9178db1b
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-09 22:21:05 +0900

    6-3-1. サーバー側のコード

--- mock_tls/cmd/main.go
+++ mock_tls/cmd/main.go
@@ -1,33 +1,53 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		fmt.Println(r.Header.Get("Connection"), r.Header.Get("Upgrade"))
 		return
 	}
-	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+	fmt.Println("Upgrade to MyProtocol")
+
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+		return
+	}
+	defer conn.Close()
+
+	response := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush()
+		recv, err := readWriter.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
 }
 
 func main() {
-	server := &http.Server{
-		TLSConfig: &tls.Config{
-			ClientAuth: tls.RequireAndVerifyClientCert,
-			MinVersion: tls.VersionTLS12,
-		},
-		Addr: ":50510",
-	}
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/upgrade", handlerUpgrade)
 	log.Println("start http listening :50510")
-	err := server.ListenAndServeTLS("server.crt", "server.key")
+	err := http.ListenAndServe(":50510", nil)
 	log.Println(err)
 }
