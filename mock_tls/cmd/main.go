f713854ed2a61e929410b12f07274c16bc15cc0c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-10 10:50:01 +0900

    6-4-1. サーバーからの送信 (チャンク)

--- mock_tls/cmd/main.go
+++ mock_tls/cmd/main.go
@@ -45,8 +45,22 @@ func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerChunkedResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk %d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
 func main() {
 	http.HandleFunc("/upgrade", handlerUpgrade)
+	http.HandleFunc("/chunked", handlerChunkedResponse)
+
 	log.Println("start http listening :50510")
 	err := http.ListenAndServe(":50510", nil)
 	log.Println(err)
